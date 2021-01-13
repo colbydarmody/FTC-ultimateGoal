07f1f7639d07369a111874eb4e2f4d8e0b37a4fc
kavthecoder <76882831+kavthecoder@users.noreply.github.com>
2021-01-12 20:18:18 -0700

    1/12 Kavya Changed package

--- teamcode/Robot/Tele-Op.java
+++ teamcode/Robot/Tele-Op.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.Robot.TeleOp;
+package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.robot.Robot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
