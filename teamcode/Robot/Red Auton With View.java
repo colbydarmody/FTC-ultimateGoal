fabff4ae77d01926f41a7afa7f7f82e2f4c445a4
CJD14147 <59951080+CJD14147@users.noreply.github.com>
2021-03-09 20:20:54 -0700

    Update Red Auton With View.java

--- teamcode/Robot/Red Auton With View.java
+++ teamcode/Robot/Red Auton With View.java
@@ -2,19 +2,27 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
 import java.util.List;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 import java.util.List;
+
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
@@ -22,22 +30,22 @@ import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 
 
-
 import android.graphics.Color;
-@Autonomous(name="Red Auto", group="Auto")
-public class Red_Autonomous extends LinearOpMode {
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+
+@Autonomous(name = "Red Auto Vuforia", group = "Auto")
 
 
-    private static final String TFOD_MODEL_ASSET = "UltimateGoal.tflite";
-    private static final String LABEL_FIRST_ELEMENT = "Quad";
-    private static final String LABEL_SECOND_ELEMENT = "Single";
+public class Red_Auton_Vuforia extends LinearOpMode {
+
+
+    public static final String TFOD_MODEL_ASSET = "UltimateGoal.tflite";
+    public static final String LABEL_FIRST_ELEMENT = "Quad";
+    public static final String LABEL_SECOND_ELEMENT = "Single";
 
     private static final String VUFORIA_KEY =
             "AYQ6ymr/////AAABmV6TulQWSE0qt3HxI7xxzjYZLt7oCOm3yO8U6uCrz/ydu705scek/RWHbSJpjJfqiTSVjL/iOtYUg7jvOcugHNMpQ/9F+0SkxFTmYxe7ts2zfCSRVa2g8N4ttFYIOyFZjfNvUJdfSVGKZy4b/TfsWq17xb5w3B+NOENkayAgTHvl6B68fuXRHocR62EZ42hFtM2fxKW3J1/9+6FxXHYA3HY/4nB1PM1NVk+e/N4Oub3u3prWcnMJFhgp4GNhPgB0AtzPHo1p3jIBy0wSHkw7Gep0Rq+i3j/9fOtJATbkCjjcHC7Jz+RkMyOBG4pSqsZi2F0FHrbekJS2igtWHp+R26m2//7Y5v0DeWtGm7pbp3y0";
-
-    private VuforiaLocalizer vuforia;
-    private TFObjectDetector tfod;
-
 
 
     /* Declare OpMode members. */
@@ -45,21 +53,33 @@ public class Red_Autonomous extends LinearOpMode {
 
     private ElapsedTime runtime = new ElapsedTime();
 
-
-
+    private VuforiaLocalizer vuforia;
+    private TFObjectDetector tfod;
 
     @Override
-    public void runOpMode() throws InterruptedException{
+    public void runOpMode() throws InterruptedException {
         // The TFObjectDetector uses the camera frames from the VuforiaLocalizer, so we create that
         // first.
-        robot.initVuforia();
-        robot.initTfod();
-
 
         /*
          * Initialize the drive system variables.
          * The init() method of the hardware class does all the work here
          */
+        initVuforia();
+        initTfod();
+
+        if (tfod != null) {
+            tfod.activate();
+
+            // The TensorFlow software will scale the input images from the camera to a lower resolution.
+            // This can result in lower detection accuracy at longer distances (> 55cm or 22").
+            // If your target is at distance greater than 50 cm (20") you can adjust the magnification value
+            // to artificially zoom in to the center of image.  For best results, the "aspectRatio" argument
+            // should be set to the value of the images used to create the TensorFlow Object Detection model
+            // (typically 16/9).
+            tfod.setZoom(2.5, 16.0/9.0);
+        }
+
         robot.init(hardwareMap);
         telemetry.addData("Mode", "calibrating...");
         telemetry.update();
@@ -67,8 +87,7 @@ public class Red_Autonomous extends LinearOpMode {
         telemetry.addData("Mode", "calibrating2...");
         telemetry.update();
 
-        while (!isStopRequested() && !robot.imu.isGyroCalibrated())
-        {
+        while (!isStopRequested() && !robot.imu.isGyroCalibrated()) {
             sleep(50);
             idle();
         }
@@ -84,7 +103,6 @@ public class Red_Autonomous extends LinearOpMode {
         robot.resetAngle();
 
 
-
         robot.reset();
 
 
@@ -97,23 +115,6 @@ public class Red_Autonomous extends LinearOpMode {
             telemetry.addData("Horizontal Encoder Position:", robot.br.getCurrentPosition());
             telemetry.update();
 
-
-
-            /**
-             * Activate TensorFlow Object Detection before we wait for the start command.
-             * Do it here so that the Camera Stream window will have the TensorFlow annotations visible.
-             **/
-            if (tfod != null) {
-                tfod.activate();
-
-                // The TensorFlow software will scale the input images from the camera to a lower resolution.
-                // This can result in lower detection accuracy at longer distances (> 55cm or 22").
-                // If your target is at distance greater than 50 cm (20") you can adjust the magnification value
-                // to artificially zoom in to the center of image.  For best results, the "aspectRatio" argument
-                // should be set to the value of the images used to create the TensorFlow Object Detection model
-                // (typically 16/9).
-                tfod.setZoom(2.5, 16.0/9.0);
-            }
 
             /** Wait for the game to begin */
             telemetry.addData(">", "Press Play to start op mode");
@@ -146,51 +147,15 @@ public class Red_Autonomous extends LinearOpMode {
             if (tfod != null) {
                 tfod.shutdown();
             }
-        }
-
-        /**
-         * Initialize the Vuforia localization engine.
-         */
-        robot.initVuforia();
-
-        /**
-         * Initialize the TensorFlow Object Detection engine.
-         */
-        robot.initTfod();
 
 
-
-//            robot.forwardByEncoder(0.5, 9000);
-
-//            robot.forward(0.5);
-//            sleep(1000);
-//            robot.stop();
-//            sleep(100);
-//
-//
-//            robot.right(0.5);
-//            sleep(1000);
-//            robot.stop();
-//            sleep(100);
-//
-//            robot.backward(0.5);
-//            sleep(1000);
-//            robot.stop();
-//            sleep(100);
-//
-//            robot.left(0.5);
-//            sleep(1000);
-//            robot.stop();
-//            sleep(100);
-
-
-            robot.backward(0.5);
+            robot.forward(0.5);
             sleep(1250);
             robot.stop();
 
 
-            robot.wrist.setPosition(0.2);
-            robot.shoulder.setPosition(0.33);
+//        robot.wrist.setPosition(0.2);
+//        robot.shoulder.setPosition(0.33);
 
             robot.backward(0.5);
             sleep(250);
@@ -198,7 +163,6 @@ public class Red_Autonomous extends LinearOpMode {
             sleep(50);
 
             // sleep(2000);
-
 
 
 //            robot.shoulder.setPosition(0);
@@ -210,8 +174,6 @@ public class Red_Autonomous extends LinearOpMode {
 //            robot.hand.setPosition(1);
 //
 //            sleep(500000);
-
-
 
 
             //////////////////////////////
@@ -253,7 +215,6 @@ public class Red_Autonomous extends LinearOpMode {
             sleep(500);
 
 
-
             robot.left(0.5);
             sleep(350);
             robot.stop();
@@ -289,9 +250,9 @@ public class Red_Autonomous extends LinearOpMode {
             sleep(50);
 
             //// drop wobble//////////////////
-            robot.shoulder.setPosition(0);
-            robot.wrist.setPosition(0.2);
-            robot.elbow.setPosition(1);
+//        robot.shoulder.setPosition(0);
+//        robot.wrist.setPosition(0.2);
+//        robot.elbow.setPosition(1);
 
             sleep(1000);
 
@@ -310,9 +271,40 @@ public class Red_Autonomous extends LinearOpMode {
 
 
             telemetry.update();
-
         }
 
-
     }
+
+
+    /**
+     * Initialize the Vuforia localization engine.
+     */
+    private void initVuforia() {
+        /*
+         * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
+         */
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraName = hardwareMap.get(WebcamName.class, "Webcam 1");
+
+        //  Instantiate the Vuforia engine
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+
+        // Loading trackables is not necessary for the TensorFlow Object Detection engine.
+    }
+
+    /**
+     * Initialize the TensorFlow Object Detection engine.
+     */
+    private void initTfod() {
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfodParameters.minResultConfidence = 0.8f;
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT, LABEL_SECOND_ELEMENT);
+    }
+
+}
 
