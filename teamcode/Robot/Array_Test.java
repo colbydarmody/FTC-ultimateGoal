76ebb62bcec17184f3a21517fe94eecbf247e703
kavthecoder <76882831+kavthecoder@users.noreply.github.com>
2021-03-27 12:08:43 -0600

    Update Array_Test.java

--- teamcode/Robot/Array_Test.java
+++ teamcode/Robot/Array_Test.java
@@ -33,12 +33,11 @@ public class Tele_Op extends LinearOpMode {
         sleep(500);
         
         int n = -1;
-        int m = 0;
         java.util.ArrayList <String>elevatorPos = new java.util.ArrayList<String>(); //array for elevator encoder values
-        java.util.ArrayList <String>elevatorVelocity = new java.util.ArrayList<String>(); //array for elevator
+        java.util.ArrayList <String>elevatorSpeed = new java.util.ArrayList<String>(); //array for elevator
         java.util.ArrayList <String>elevatorAccel = new java.util.ArrayList<String>();
         //all arrays are completely empty at start
-        elevatorVelocity.add("0"); //element 0 of elevatorVelocity will not be used
+        elevatorSpeed.add("0"); //element 0 of elevatorVelocity will not be used
         elevatorAccel.add("0"); //element 0 of elevatorAccel will not be used
         elevatorAccel.add("0"); //element 1 of elevatorAccel will not be used
         
@@ -50,15 +49,18 @@ public class Tele_Op extends LinearOpMode {
             
             ////////////// Elevator/////////////////
             if (gamepad2.left_stick_y > 0.2) {
+                robot.elevator.setPower(gamepad2.left_stick_y);
                 elevatorPos.add(String.valueOf(robot.elevator.getCurrentPosition)); //add encoder value to the array
                 n++; //updating element # so we can reference it later
-                if n > 0; //we need at least two encoder values to calculate velocity, so this ensures that we have that
-                    elevatorVelocity.add(String.valueOf);
-                //if (robot.elevator.getCurrentPosition() > -1175) {
-                    //robot.elevator.setPower(gamepad2.left_stick_y);
+                if (n > 0) { //we need at least two encoder values to calculate velocity, so this ensures that we have that
+                    elevatorSpeed.add(String.valueOf((elevatorPos.get(n)-elevatorPos.get(n-1))/(/*time*/)));
+                    if (n > 1) {
+                        elevatorAccel.add(String.valueOf((elevatorSpeed.get(n)-elevatorSpeed.get(n-1))/(/*time*/))))
+                    }
                 }
-
+                //if (robot.elevator.getCurrentPosition() > -1175) {
             }
+
             if (gamepad2.left_stick_y < -0.2) {
                 if (robot.elevator.getCurrentPosition() < 0) {
                     robot.elevator.setPower(gamepad2.left_stick_y);
